a517331cf54ba198a742ad9352f93a0e24f9ca8c
Marcos Rodrigues <122711459+Marcos-Rodrigues-JS@users.noreply.github.com>
2023-01-22 00:42:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-function consulta() { 
-
-const cep = document.querySelector("#cep")
-const numeroCEP = cep.value
-const showData = (result)=>{
-    for(const campo in result){
-        if(document.querySelector("#"+campo)){
-            document.querySelector("#"+campo).value = result[campo]
+async function consulta() { 
+    const cep = document.querySelector("#cep")
+    const numeroCEP = (cep.value).replace("-","")
+    if (cep.length > 9) {
+        window.alert("[Erro] - Por favor digite o formato de cep, são apenas 8 dígitos");
+        cep.value = "";
+    }
+    const showData = (result) => {
+        for(const campo in result){
+            if(document.querySelector("#"+campo)){
+                document.querySelector("#"+campo).value = result[campo]
+            }
         }
     }
-}
-const option = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'default'
-}
-    
-fetch(`https://viacep.com.br/ws/${numeroCEP}/json/`, option)
-.then((response)=>{response.json()
-    .then(data => showData(data))
-})
-.catch(error => {
-    if (error.response == undefined){
-        window.alert("[ERRO] - CEP NÃO ENCONTRADO")
-        cep.value = ""
+    const option = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
     }
-     
-})}
 
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${numeroCEP}/json/`, option);
+        const data = await response.json();
+        if (!data) {
+            throw new Error("Não foi possível encontrar um endereço com esse CEP");
+        }
+        showData(data);
+    } catch (error) {
+        window.alert("[ERRO] - " + error.message);
+    }
+}
